a56a61191c3baa984302d9fb6e8eb006e7b446cc
Jaryl Sim <jaryl.sim@me.com>
2022-11-01 13:33:08 +0800

    Second session of mob programming.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,17 +8,37 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   root to: "home_page#welcome"
 
-  resources :posts, only: [:index, :show]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
